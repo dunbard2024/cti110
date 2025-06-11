d23bcfdc78ee665b4b71138a3cca2fdeec8c4967
dunbard2024 <dunbard4623@student.faytechcc.edu>
2025-06-11 10:53:35 -0400

    Add files via upload

---
+++ P1LAB1_dunbar_dustin.py
@@ -0,0 +1,11 @@
+# Dustin Dunbar
+# 20250611
+# P1LAB1
+# Writing simple python
+f_name='Dustin'
+l_name='Dunbar'
+w_name='Dustin Dunbar!'
+print('Enter your first name:',f_name)
+print('Enter your last name:',l_name)
+print('Hello,',w_name,'Welcome to CTI-110')
+
