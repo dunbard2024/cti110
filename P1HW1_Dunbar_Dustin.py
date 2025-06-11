aa25a82d3be81de18db22cfe79a03c2b9afccfa3
DunbarFTCC <dunbard4623@student.faytechcc.edu>
2025-06-11 12:21:02 -0400

    Add files via upload
    P1HW1

---
+++ P1HW1_Dunbar_Dustin.py
@@ -0,0 +1,25 @@
+# Dustin Dunbar
+# June 11,2025
+# P1HW1
+# Using Python's input and print functions to recreate P1HW1.
+print('-----Caculating Exponenets-----')
+base_value = 7
+exponent = 3
+print('Enter an integer as the base value:',base_value)
+print('Enter an integer as the exponent:',exponent)
+result = base_value ** exponent
+print(base_value,'rasied to the power of',exponent,'is',result,'!!')
+
+
+print('-----Addition and Subtractrion----')
+
+a = 10
+b = 6
+c = 4
+
+print('Enter a starting integer:',a)
+print('Enter an integer to add:',b)
+print('Enter an integer to subtract:',c)
+sum = a+b-c
+
+print(a,'+',b,'-',c,'is equal to',sum)
